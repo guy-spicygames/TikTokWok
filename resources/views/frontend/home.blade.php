90e4863fea4d7a29bedadf3776c3a45e342b6172
manjumohan87 <55729533+manjumohan87@users.noreply.github.com>
2022-12-31 12:21:46 +0530

    #15

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -429,41 +429,85 @@
                 </div>
             </section>
             <!-- about  section -->
+            
 
             <!-- Modal Orderform -->
-
             <div class="modal fade menumodal" id="OrderformModal" tabindex="-1" aria-labelledby="OrderformModalLabel" aria-hidden="true">
                 <div class="modal-dialog vertical-align-center">
                     <div class="modal-content">
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="500px" height="500px" viewBox="0 0 500 500" enable-background="new 0 0 500 500" xml:space="preserve">
-                                    <path d="M265,251.2l205-205c3.8-3.8,3.8-11.2,0-15c-3.7-3.8-11.2-3.8-15,0l-205,205L45,31.2c-3.8-3.8-11.2-3.8-15,0
-	                                                s-3.8,11.2,0,15l205,205l-205,205c-3.8,3.8-3.8,11.2,0,15c2.5,1.3,5,2.5,7.5,2.5s5-1.2,7.5-2.5l205-205l205,205
-	                                                c2.5,1.3,5,2.5,7.5,2.5s5-1.2,7.5-2.5c3.8-3.8,3.8-11.2,0-15L265,251.2z"></path>
-
-                                </svg>
+                                
+                                <img src="{{asset('public/assets/frontend/assets/images/close-btn.png')}}" alt="img">   
                             </button>
                         </div>
                         <div class="modal-body">
                             <div class="image-div">
-                                <img src="{{asset('public/assets/frontend/assets/images/content/gallery-img2.jpg')}}" alt="img">                                
+                                <img src="{{asset('public/assets/frontend/assets/images/content/orderform-img.jpg')}}" alt="img">                                
                             </div>
 
                             <div class="button-group">
                                 <button type="submit" class="bttn"> טעינת כרטיס</button> {{-- Card loading --}}                                
-                                <button type="submit" class="bttn">משלוח</button>{{-- Shipping --}}                                
+                                <button type="submit" class="bttn shippingbtn" data-toggle="modal" data-target="#ShippingformModal" data-dismiss="modal" aria-label="Close">משלוח</button>{{-- Shipping --}}                                
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
             <!-- Modal Orderform -->
+            
+             <!-- Modal shippingform -->
+            <div class="modal fade menumodal" id="ShippingformModal" tabindex="-1" aria-labelledby="ShippingformModalLabel" aria-hidden="true">
+                <div class="modal-dialog vertical-align-center">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">                               
+                                
+                                <img src="{{asset('public/assets/frontend/assets/images/close-btn.png')}}" alt="img">   
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                          <form id="address-form" action="" method="get" autocomplete="off">                         
+                          <p class="note"><em>* = required field</em></p>
+                          <label class="full-field">                         
+                            <span class="form-label">Deliver to*</span>
+                            <input
+                              id="ship-address"
+                              name="ship-address"
+                              required
+                              autocomplete="off"
+                            />
+                          </label>
+                          <label class="full-field">
+                            <span class="form-label">Apartment, unit, suite, or floor #</span>
+                            <input id="address2" name="address2" />
+                          </label>
+                          <label class="full-field">
+                            <span class="form-label">City*</span>
+                            <input id="locality" name="locality" required />
+                          </label>
+                          <label class="slim-field-left">
+                            <span class="form-label">State/Province*</span>
+                            <input id="state" name="state" required />
+                          </label>
+                          <label class="slim-field-right" for="postal_code">
+                            <span class="form-label">Zip code*</span>
+                            <input id="postcode" name="postcode" required />
+                          </label>
+                          <label class="full-field">
+                            <span class="form-label">Country/Region*</span>
+                            <input id="country" name="country" required />
+                          </label>
+                          <button type="button" class="bttn">Save address</button>
+                          <input type="reset" value="Clear form" />
+                        </form>
 
 
-
-
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Modal shippingform -->
 
 
             <!-- celebrate  section -->
@@ -916,9 +960,95 @@
     <script src="{{asset('public/assets/frontend/assets/js/aos.js')}}"></script>
     <script src="{{asset('public/assets/frontend/assets/js/jquery.easytabs.min.js')}}"></script>
     <script src="{{asset('public/assets/frontend/assets/js/slick.min.js')}}"></script>
-    <script src="{{asset('public/assets/frontend/assets/js/lightbox.js')}}"></script>
+    <script src="{{asset('public/assets/frontend/assets/js/lightbox.js')}}"></script>  
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&callback=initAutocomplete&libraries=places&v=weekly" defer
+    ></script>
     <script src="{{asset('public/assets/frontend/assets/js/main.js')}}"></script>
     
+    
+    <script>  
+    let autocomplete;
+    let address1Field;
+    let address2Field;
+    let postalField;
+
+    function initAutocomplete() {
+      address1Field = document.querySelector("#ship-address");
+      address2Field = document.querySelector("#address2");
+      postalField = document.querySelector("#postcode");
+      // Create the autocomplete object, restricting the search predictions to
+      // addresses in the US and Canada.
+      autocomplete = new google.maps.places.Autocomplete(address1Field, {
+        componentRestrictions: { country: ["us", "ca"] },
+        fields: ["address_components", "geometry"],
+        types: ["address"],
+      });
+      address1Field.focus();
+      // When the user selects an address from the drop-down, populate the
+      // address fields in the form.
+      autocomplete.addListener("place_changed", fillInAddress);
+    }
+
+    function fillInAddress() {
+      // Get the place details from the autocomplete object.
+      const place = autocomplete.getPlace();
+      let address1 = "";
+      let postcode = "";
+
+      // Get each component of the address from the place details,
+      // and then fill-in the corresponding field on the form.
+      // place.address_components are google.maps.GeocoderAddressComponent objects
+      // which are documented at http://goo.gle/3l5i5Mr
+      for (const component of place.address_components) {
+        // @ts-ignore remove once typings fixed
+        const componentType = component.types[0];
+
+        switch (componentType) {
+          case "street_number": {
+            address1 = `${component.long_name} ${address1}`;
+            break;
+          }
+
+          case "route": {
+            address1 += component.short_name;
+            break;
+          }
+
+          case "postal_code": {
+            postcode = `${component.long_name}${postcode}`;
+            break;
+          }
+
+          case "postal_code_suffix": {
+            postcode = `${postcode}-${component.long_name}`;
+            break;
+          }
+          case "locality":
+            document.querySelector("#locality").value = component.long_name;
+            break;
+          case "administrative_area_level_1": {
+            document.querySelector("#state").value = component.short_name;
+            break;
+          }
+          case "country":
+            document.querySelector("#country").value = component.long_name;
+            break;
+        }
+      }
+
+      address1Field.value = address1;
+      postalField.value = postcode;
+      // After filling the form with address components from the Autocomplete
+      // prediction, set cursor focus on the second address line to encourage
+      // entry of subpremise information such as apartment, unit, or floor number.
+      address2Field.focus();
+    }
+
+    window.initAutocomplete = initAutocomplete;
+        
+        
+    </script>
 
 </body>
 
